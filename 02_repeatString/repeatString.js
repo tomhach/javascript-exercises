26609e1ed044e9a9a2bb278b42b4f1195a940c1c
tomhach <hughes.ta@hotmail.com>
2023-08-31 20:04:31 +0100

    03 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,15 +1,7 @@
 const repeatString = function(outputString, count) {
-    if(count < 0)
-    {
-        return "ERROR";
-    }
-    if(count == 0)
-    {
-        return "";
-    }
-
+    if(count < 0) return "ERROR";
+    if(count == 0) return "";
     return outputString + repeatString(outputString, count - 1);
 };
-
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let reversedString = "";
+    for(let s = stringToReverse.length - 1; s >= 0; s--)
+    {
+        reversedString += stringToReverse[s];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
