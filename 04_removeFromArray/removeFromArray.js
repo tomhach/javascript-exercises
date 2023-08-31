93aa7285e575186d1846f8a7fcae7a7438bf2df7
tomhach <hughes.ta@hotmail.com>
2023-08-31 22:51:04 +0100

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    const newArray = [];
+    array.forEach((item) => {
+        if (!values.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
