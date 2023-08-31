1fb75050992641e27380e38dc62d6af80ba01003
tomhach <hughes.ta@hotmail.com>
2023-08-31 19:45:48 +0100

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(outputString, count) {
+    if(count < 0)
+    {
+        return "ERROR";
+    }
+    if(count == 0)
+    {
+        return "";
+    }
 
+    return outputString + repeatString(outputString, count - 1);
 };
 
 // Do not edit below this line
