82c41d6199c3820a3db18a27bc0e55786570da3a
tomhach <hughes.ta@hotmail.com>
2023-08-31 23:15:24 +0100

    03 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(value1, value2) {
+    if(value1 < 0 || value2 < 0)
+    {
+        return "ERROR";
+    }
+    if(!Number.isInteger(value1) || !Number.isInteger(value2))
+    {
+        return "ERROR";
+    }
 
+
+
+    let newValue = 0;
+    if(value1 < value2)
+    {
+        for(value1; value1 <= value2; value1++)
+        {
+            newValue += value1;        
+        }
+    }
+    else
+    {
+        for(value2; value2 <= value1; value2++)
+        {
+            newValue += value2;        
+        }
+    }
+    return newValue;
 };
 
 // Do not edit below this line
