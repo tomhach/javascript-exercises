4eba478110adc6d55b3904c44971d820a005ba73
tomhach <hughes.ta@hotmail.com>
2023-09-04 22:07:25 +0100

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(v1, v2) {
+    return v1 + v2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(v1, v2) {
+    return v1 - v2;	
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+    let result = 0;
+    for(let x = 0; x < args.length; x++)
+    {
+        result += args[x];
+    }
+    return result;
 };
 
-const multiply = function() {
+const multiply = function(args) {
+    let result = 1;
+    for(let x = 0; x < args.length; x++)
+    {
+        result *= args[x];
+    }
+    return result;
 
 };
 
-const power = function() {
-	
+const power = function(args, pwr) {
+    let result = 1;
+    for(let x = 0; x < pwr; x++)
+    {
+        result *= args;
+    }
+    return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	let result = 1;
+    for(value; value > 0; value--)
+    {
+        result *= value;
+    }
+    return result;
 };
 
 // Do not edit below this line
