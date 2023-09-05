b03166cf40a5e2dd3fbd906fcf8202f4682f5a7b
tomhach <hughes.ta@hotmail.com>
2023-09-05 20:50:38 +0100

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 0;
+    for (i = 1; i < num; i++) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return num2;
 };
 
 // Do not edit below this line
